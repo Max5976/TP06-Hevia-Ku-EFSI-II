4595696a6140eb95c789ae10a91428cbcfde6469
Max <95097703@est.ort.edu.ar>
2026-05-18 14:30:07 -0300

    Avanzar en APp

---
+++ src/api/the-cat-api.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://thecatapi.com/',
+  params: {
+    apikey: 'live_cPuQXIkdZN2xFBocp2vOsde1hGswMwDnTJwI18ryYsyjFxDq87qwTOQVhTCQNE77'
+  }
+});
+
+export default api;
